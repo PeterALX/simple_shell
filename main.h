31dd224440072a55642affc6a658a3929a2cf44f
DennisGicheru <dennisgicheru698@gmail.com>
2022-11-11 12:44:06 -0800

    env command

--- main.h
+++ main.h
@@ -5,8 +5,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 extern char **environ;
 
 char **tokenize(char *);
+int strcompare(char *str1, char *str2);
+
+void printstr(char **str_arr);
+
 #endif
