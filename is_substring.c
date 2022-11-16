f69eab38e5da2e57d3cfed6585f62f6b46b2b824
PeterALX <quaypeter25@gmail.com>
2022-11-16 14:17:27 +0000

    finish handling memory leaks

--- is_substring.c
+++ is_substring.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-*is_substring - checks if str2 begins with str1 
-*@str1: string 1 
+*is_substring - checks if str2 begins with str1
+*@str1: string 1
 *@str2: string 2
 *Return: 0 if success, 1 if not success
 */
@@ -17,9 +17,9 @@ int is_substring(char *str1, char *str2)
 			return (0);
 		n++;
 	}
-	
+
 	if (str1[n] != '\0')
 		return (0);
-	
+
 	return (1);
 }
