fe99a3af679e518fffb1b9de43f97b68b8617e01
PeterALX <quaypeter25@gmail.com>
2022-11-16 12:52:10 +0000

    start handling memory leaks

--- _strcpy.c
+++ _strcpy.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
-* _strcpy - copies a string 
+* _strcpy - copies a string
 * @src: the string to be copied
 * Return: a pointed to the copy
 */
 char *_strcpy(char *src)
 {
 	char *copy;
+	char *tmp;
 	int i = 0;
 	int n = 200;
-	
+
 	copy = malloc(sizeof(char) * 200);
 	if (copy == NULL)
 		return (copy);
+
+	tmp = copy;
 
 	while (src[i])
 	{
@@ -21,8 +24,13 @@ char *_strcpy(char *src)
 		{
 			n += 5;
 			copy = realloc(copy, sizeof(char) * n);
+			if (copy == NULL)
+			{
+				free(tmp);
+				return (copy);
+			}
 		}
-		
+
 		copy[i] = src[i];
 		i++;
 	}
--- comparestr.c
+++ comparestr.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-*strcompare - Compares strings 
-*@str1: string 1 
+*strcompare - Compares strings
+*@str1: string 1
 *@str2: string 2
 *Return: 0 if success, 1 if not success
 */
@@ -17,9 +17,9 @@ int strcompare(char *str1, char *str2)
 			return (0);
 		n++;
 	}
-	
+
 	if (str1[n] != '\0' || str2[n] != '\0')
 		return (0);
-	
+
 	return (1);
 }
